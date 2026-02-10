70949372edb47d19d062f4fc297be7c9c8182473
Aizaz Khan <aizazkhan7874@gmail.com>
2026-02-10 23:09:57 +0500

    based model initialized

---
+++ base_model.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+
+# I am using InceptionV3 model as my base model
+# initilizing base model
+def pre_trained_model():
+    Base_Model = tf.keras.applications.inception_v3.InceptionV3(
+        include_top = False,
+        input_shape = (150, 150, 3),
+        weights = 'imagenet'
+    )
+
+    Base_Model.trainable = False
+
+    return Base_Model
+
+if __name__ == '__main__':
+    pre_trained_model()
+    print('Base Model Successfully Initilized')
