5a4f3b52f26b4401e4a47ef16dd584a0dafae57f
Aizaz Khan <aizazkhan7874@gmail.com>
2026-02-13 18:33:24 +0500

    getting layer mixed7 as last layer from base model

---
+++ cutting_layer.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from base_model import pre_trained_model
+
+base_model = pre_trained_model()
+
+# cutting the layer 'mixed7' inception block
+def getting_layer():
+
+    last_layer_of_base_model = base_model.get_layer('mixed7')
+    last_output = last_layer_of_base_model.output
+    print(f'output of last layer of base model : {last_layer_of_base_model.output}')
+
+if __name__ == '__main__':
+    getting_layer()
