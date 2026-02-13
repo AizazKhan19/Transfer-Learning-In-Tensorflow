2dd9cd23c9205e2dca2f9b2f6c604796d2fea656
Aizaz Khan <aizazkhan7874@gmail.com>
2026-02-13 19:03:58 +0500

    taking last output out of the function to access it another file

--- cutting_layer.py
+++ cutting_layer.py
@@ -4,10 +4,11 @@ from base_model import pre_trained_model
 base_model = pre_trained_model()
 
 # cutting the layer 'mixed7' inception block
-def getting_layer():
+last_layer_of_base_model = base_model.get_layer('mixed7')
+last_output = last_layer_of_base_model.output
 
-    last_layer_of_base_model = base_model.get_layer('mixed7')
-    last_output = last_layer_of_base_model.output
+def getting_layer():
+    
     print(f'output of last layer of base model : {last_layer_of_base_model.output}')
 
 if __name__ == '__main__':
